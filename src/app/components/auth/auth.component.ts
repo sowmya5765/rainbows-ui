601309154a1ab471d39d8bd3aec1967570575d68
Sowmya <sowmya.kasireddy@merittrac.com>
2024-02-18 23:23:30 +0530

    logout

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -8,7 +11,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -16,6 +19,25 @@ export class AuthComponent implements OnInit {
   login(authForm: NgForm){
     console.log("Username:", authForm.value.username);
     console.log("Password:", authForm.value.password);
+    let body:any ={}
+    if(authForm.value.username && authForm.value.password){
+      body.emailId = authForm.value.username;
+      body.password = authForm.value.password;
+    }else{
+      alert(`Enter email and password!!`)
+    }
+    this.loginApi(body).subscribe((res:any)=>{
+      console.log("resuuuuuuuu",res.data);
+      localStorage.setItem('authorization',res.data.accessToken)
+      localStorage.setItem('userId',res.data.userId)
+      this.router.navigate(['/']);
+    },(err)=>{
+      alert('Failed To Login')
+    })
+  }
+
+  loginApi(body:any){
+    return this.http.post(`${environment.serviceUrl}/admin/login`,body)
   }
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartServiceService } from '../services/cart-service.service';
 import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -12,14 +14,38 @@ export class HeaderComponent implements OnInit {
   userCartData:any;
   cartTotalAmt:any;
   isDropdownOpen: boolean = false;
+  SignInStatus = 'Login';
 
   constructor(public cartService:CartServiceService,
-    private router: Router) { }
+    private router: Router,private http:HttpClient) { }
 
   ngOnInit(): void {
     this.cartService.cartTotal.subscribe(total=>this.cartTotalAmt = total);
     this.cartService.cartDataBS.subscribe(data=>this.userCartData = data);
-    console.log("userCartData.items[0].inCart !== 0",this.userCartData,this.userCartData.items[0].inCart !== 0)
+    if(localStorage.getItem('authorization')){
+      this.SignInStatus = 'Logout'
+    }
+  }
+
+  userSignInOrSignOut(){
+    if(this.SignInStatus=='Logout'){
+      let body:any ={};
+      if(localStorage.getItem('userId')){
+        body.id = localStorage.getItem('userId');
+      }
+      this.loginOut(body).subscribe((res:any)=>{
+        console.log("resuuuuuuuu",res.data);
+        localStorage.removeItem('authorization');
+        this.SignInStatus = 'Login';
+        this.router.navigate(['/']);
+      })
+    }else{
+        this.router.navigate(['/auth']);
+    }
+  }
+
+  loginOut(body:any){
+    return this.http.post(`${environment.serviceUrl}/admin/logout`,body)
   }
 
   toggleDropdown() {
